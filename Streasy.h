706c9af568b619205f50413a00d2649af34d820c
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2022-12-04 22:45:06 +0300

    Update Streasy.h

--- Streasy.h
+++ Streasy.h
@@ -4,6 +4,10 @@
 #include<string>
 using namespace std;
 
-string itc_hello_str(string n);
+string itc_hello_str(string n); //Заказные приветы (№1)
+long long itc_len(string n); //Считала символов (№2)
+void itc_print_copy_str(string n, int a); //Указанный символ из строки (№3)
+void itc_first_end_three(string n); //первые и последние 3 символа/первый символ n раз (№4)
+int itc_count_char_in_str(char b, string n); //Считатель символа в строке (№5)
 
 #endif // STREASY_H_INCLUDED
